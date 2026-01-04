634e7eaff86c8f579ce282449bef9e7697f2fd1f
Denis Mishankov <mishankov@mail.com>
2026-01-05 00:52:44 +0300

    add validation

--- src/routes/settings/EditProviderModal.svelte
+++ src/routes/settings/EditProviderModal.svelte
@@ -10,14 +10,22 @@
 
 	let editModal = $state<HTMLDialogElement>();
 	let localProvider = $state<Provider>({ ...provider });
+	let validateMessages = $state<{
+		name?: string;
+		baseUrl?: string;
+		apiKey?: string;
+		models?: string[];
+	}>({});
 
 	const onSave = async () => {
-		await fetch(resolve('/settings'), {
-			method: 'POST',
-			body: JSON.stringify(localProvider)
-		});
-		window.location.reload();
-		editModal?.close();
+		if (validate()) {
+			await fetch(resolve('/settings'), {
+				method: 'POST',
+				body: JSON.stringify(localProvider)
+			});
+			window.location.reload();
+			editModal?.close();
+		}
 	};
 
 	const onDelete = async () => {
@@ -29,8 +37,49 @@
 		editModal?.close();
 	};
 
+	const validate = () => {
+		let valid = true;
+
+		if (localProvider.name.length < 1) {
+			valid = false;
+			validateMessages.name = 'Name is required';
+		} else {
+			validateMessages.name = '';
+		}
+
+		if (localProvider.baseUrl.length < 1) {
+			valid = false;
+			validateMessages.baseUrl = 'Base URL is required';
+		} else {
+			validateMessages.baseUrl = '';
+		}
+
+		if (localProvider.apiKey.length < 1) {
+			valid = false;
+			validateMessages.apiKey = 'API key is required';
+		} else {
+			validateMessages.apiKey = '';
+		}
+
+		// Validate model IDs
+		if (localProvider.models) {
+			validateMessages.models = [];
+			localProvider.models.forEach((model, index) => {
+				if (!model.id || model.id.length < 1) {
+					valid = false;
+					validateMessages.models![index] = 'Model ID is required';
+				} else {
+					validateMessages.models![index] = '';
+				}
+			});
+		}
+
+		return valid;
+	};
+
 	export const open = () => {
 		localProvider = structuredClone($state.snapshot(provider));
+		validateMessages = {};
 		editModal?.showModal();
 	};
 </script>
@@ -42,20 +91,35 @@
 		</form>
 
 		<h4 class="text-xl">Provider info</h4>
-		<label class="input w-full">
-			<span class="label">Name <span class="text-error">*</span></span>
-			<input type="text" bind:value={localProvider.name} />
-		</label>
+		<div>
+			<label class={[validateMessages.name && 'input-error', 'input w-full']}>
+				<span class="label">Name <span class="text-error">*</span></span>
+				<input type="text" bind:value={localProvider.name} required />
+			</label>
+			{#if validateMessages.name}
+				<div class="text-xs text-error">{validateMessages.name}</div>
+			{/if}
+		</div>
 
-		<label class="input w-full">
-			<span class="label">Base URL <span class="text-error">*</span></span>
-			<input type="text" bind:value={localProvider.baseUrl} />
-		</label>
+		<div>
+			<label class={[validateMessages.baseUrl && 'input-error', 'input w-full']}>
+				<span class="label">Base URL <span class="text-error">*</span></span>
+				<input type="text" bind:value={localProvider.baseUrl} required />
+			</label>
+			{#if validateMessages.baseUrl}
+				<div class="text-xs text-error">{validateMessages.baseUrl}</div>
+			{/if}
+		</div>
 
-		<label class="input w-full">
-			<span class="label">API key <span class="text-error">*</span></span>
-			<input type="password" bind:value={localProvider.apiKey} />
-		</label>
+		<div>
+			<label class={[validateMessages.apiKey && 'input-error', 'input w-full']}>
+				<span class="label">API key <span class="text-error">*</span></span>
+				<input type="password" bind:value={localProvider.apiKey} required />
+			</label>
+			{#if validateMessages.apiKey}
+				<div class="text-xs text-error">{validateMessages.apiKey}</div>
+			{/if}
+		</div>
 
 		{#if localProvider.id}
 			<button class="btn w-30 btn-xs btn-error" onclick={onDelete}>Delete provider</button>
@@ -75,10 +139,15 @@
 			<ul class="list">
 				{#each localProvider.models as model, i (i)}
 					<li class="list-row flex flex-col gap-2 pr-0 pl-0">
-						<label class="input w-full">
-							<span class="label">ID <span class="text-error">*</span></span>
-							<input type="text" bind:value={model.id} required />
-						</label>
+						<div>
+							<label class={[validateMessages.models?.[i] && 'input-error', 'input w-full']}>
+								<span class="label">ID <span class="text-error">*</span></span>
+								<input type="text" bind:value={model.id} required />
+							</label>
+							{#if validateMessages.models?.[i]}
+								<div class="text-xs text-error">{validateMessages.models[i]}</div>
+							{/if}
+						</div>
 
 						<label class="input w-full">
 							<span class="label">Name</span>
